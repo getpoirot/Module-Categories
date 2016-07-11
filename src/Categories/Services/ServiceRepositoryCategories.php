d49bf82206221c8ac6212b9cc40a971d97661360
Payam Naderi <naderi.payam@gmail.com>
2016-07-11 11:17:50 +0430

    extend abstract repo service factory

--- src/Categories/Services/ServiceRepositoryCategories.php
+++ src/Categories/Services/ServiceRepositoryCategories.php
@@ -2,75 +2,43 @@
 namespace Module\Categories\Services;
 
 use Module\Categories\Model\Repository;
-use Module\MongoDriver\MongoDriverManagementFacade;
 
-use Poirot\Application\aSapi;
-use Poirot\Ioc\Container\Service\aServiceContainer;
-use Poirot\Std\Struct\DataEntity;
+use Module\MongoDriver\Services\aServiceRepository;
 
 /*
 $categories = $services->fresh(
-    '/modules/categories/services/repository/categories'
+    '/module/categories/services/repository/categories'
     , ['db_collection' => 'trades.categories'] // override options
 );
 $r = $categories->getTree($categories->findByID('red'));
 */
 
 class ServiceRepositoryCategories 
-    extends aServiceContainer
+    extends aServiceRepository
 {
     /** @var string Service Name */
     protected $name = 'categories';
-    
+
+
     /**
-     * Create Service
+     * Repository Class Name
+     *   Module\Categories\Model\Repository\Categories
      *
-     * @return mixed
+     * @return string
      */
-    function newService()
+    function getRepoClassName()
     {
-        $services = $this->services();
-
-        $this->__prepareOptions();
-
-        /** @var MongoDriverManagementFacade $mongoDriver */
-        $mongoDriver   = $services->get('/module/mongoDriver');
-        $db            = $mongoDriver->query($this->optsData()->getMongoClient());
-        $categoriesRepo = new Repository\Categories($db, $this->optsData()->getDbCollection());
-        return $categoriesRepo;
+        return \Module\Categories\Model\Repository\Categories::class;
     }
 
     /**
-     * Retrieve and merge options from application merged config
-     * @throws \Exception
+     * Get Key Of Merged Config To Retrieve Settings
+     *  \Module\Categories\Module::CONF_KEY
+     *
+     * @return string
      */
-    protected function __prepareOptions()
+    function getMergedConfKey()
     {
-        $services    = $this->services();
-
-        /** @var aSapi $config */
-        $config       = $services->get('/sapi');
-        $config       = $config->config();
-        /** @var DataEntity $config */
-        $config       = $config->get(\Module\Categories\Module::CONF_KEY, array());
-
-        if (!$this->optsData()->getMongoClient()) {
-            $mongoClient = (isset($config['mongo_client']))
-                ? $config['mongo_client']
-                : MongoDriverManagementFacade::CLIENT_DEFAULT;
-
-            $this->optsData()->setMongoClient($mongoClient);
-        }
-
-        if (!$this->optsData()->getDbCollection()) {
-            $mongoCollection = (isset($config['db_collection']))
-                ? $config['db_collection']
-                : null;
-
-            if (!$mongoCollection)
-                throw new \Exception('DB Collection name for categories not defined.');
-
-            $this->optsData()->setDbCollection($mongoCollection);
-        }
+        return \Module\Categories\Module::CONF_KEY;
     }
 }
