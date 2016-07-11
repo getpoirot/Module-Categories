b7470a7c54b3ecbd0ba1b5635ee90084d382dcc0
Payam Naderi <naderi.payam@gmail.com>
2016-07-11 11:17:56 +0430

    code cleanup

--- src/Categories/Services/Repository/IOC.php
+++ src/Categories/Services/Repository/IOC.php
@@ -4,8 +4,12 @@ namespace Module\Categories\Services\Repository;
 use Module\Categories\Model\Repository\Categories;
 
 /**
+ * Usage:
+ *   to ease access to IoC nested containers
+ *   Module\Categories\Services\Repository\IOC::categories()
+ * 
  * @method static Categories categories(array $options=null)
- * @see \Module\Categories\Services
+ * @see \Module\Categories\Services\ServiceRepositoryCategories
  */
 class IOC extends \IOC
 { }
