664ca89ad61b8f15dc0d2d3f34a1f283bb8acd9d
Payam Naderi <naderi.payam@gmail.com>
2016-07-12 10:09:42 +0430

    fix mongo-driver configuration

--- config/module.conf.php
+++ config/module.conf.php
@@ -2,7 +2,12 @@
 return array(
     \Module\Categories\Module::CONF_KEY 
         => array(
-            'mongo_client'  => \Module\MongoDriver\MongoDriverManagementFacade::CLIENT_DEFAULT,
-            'db_collection' => 'categories',
+            \Module\MongoDriver\Services\aServiceRepository::CONF_KEY => array(
+                 'client' => \Module\MongoDriver\MongoDriverManagementFacade::CLIENT_DEFAULT,
+                 'collection' => array(
+                    'name'    => 'categories',
+                    'indexes' => array()
+                 )
+            ),
         ),
 );
