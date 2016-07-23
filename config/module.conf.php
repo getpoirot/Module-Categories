14fce3d7a106073c24a02ddc25561f36f0c0dc92
Payam Naderi <naderi.payam@gmail.com>
2016-07-23 17:55:18 +0430

    improve config repos

--- config/module.conf.php
+++ config/module.conf.php
@@ -1,10 +1,15 @@
 <?php
 return array(
     Module\MongoDriver\Module::CONF_KEY => array(
-        \Module\Categories\Services\ServiceRepositoryCategories::CONF_KEY => array(
-            'collections' => array(
-                // query on which collection
-                'categories' => array(
+        'repositories' => array(
+            // Configuration of Repository Service.
+            // Usually Implemented with modules that implement mongo usage
+            // with specific key name as repo name.
+            // @see aServiceRepository bellow
+            \Module\Categories\Services\ServiceRepositoryCategories::class => array(
+                'collection' => array(
+                    // query on which collection
+                    'name' => 'categories',
                     // which client to connect and query with
                     'client' => \Module\MongoDriver\MongoDriverManagementFacade::CLIENT_DEFAULT,
                     // ensure indexes
--- src/Categories/Services/ServiceRepositoryCategories.php
+++ src/Categories/Services/ServiceRepositoryCategories.php
@@ -16,12 +16,8 @@ $r = $categories->getTree($categories->findByID('red'));
 class ServiceRepositoryCategories 
     extends aServiceRepository
 {
-    const CONF_KEY = 'categories-repo';
-
     /** @var string Service Name */
     protected $name = 'categories';
-
-    protected $default_collection = 'categories';
 
     /**
      * Repository Class Name
@@ -32,16 +28,5 @@ class ServiceRepositoryCategories
     function getRepoClassName()
     {
         return \Module\Categories\Model\Repository\Categories::class;
-    }
-
-    /**
-     * Get Key Of Merged Config To Retrieve Settings
-     *  \Module\Categories\Module::CONF_KEY
-     *
-     * @return string
-     */
-    function getMergedConfKey()
-    {
-        return self::CONF_KEY;
     }
 }
